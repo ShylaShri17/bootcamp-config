e11fab8472ad83a5ec409d27bb0d4f285211295a
user <shylashri>
2023-04-14 14:48:54 +0100

    chapter4 prod changes

--- config/transformations/ts_assets_mapping/transformation.sql
+++ config/transformations/ts_assets_mapping/transformation.sql
@@ -1,5 +1,2 @@
-SELECT ts.externalId AS externalId,
-         ast.id AS assetId
-  FROM `_cdf`.`timeseries` AS ts
-
-  INNER JOIN `_cdf`.`assets` AS ast ON split(ts.externalId, ":") [0] = ast.externalId
+query:
+    file: transformation.sql
